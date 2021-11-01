686c7327c39287795ffd021442c85664849de249
WillBallentine <89371289+WillBallentine@users.noreply.github.com>
2021-10-31 21:54:15 -0400

    Add files via upload

---
+++ pdftotext.ps1
@@ -0,0 +1,30 @@
+﻿Start-Process powershell.exe -Verb RunAs {
+
+    function convert-PDFtoText {
+        param(
+            [Parameter(Mandatory=$true)][string]$file
+        )
+        Add-Type -Path "C:\ps\itextsharp.dll"
+        $pdf = New-Object iTextSharp.text.pdf.pdfreader -ArgumentList $file
+        for ($page = 1; $page -le $pdf.NumberOfPages; $page++){
+            $text=[iTextSharp.text.pdf.parser.PdfTextExtractor]::GetTextFromPage($pdf,$page)
+            Write-Output $text
+        }
+        $pdf.Close()
+    }
+
+
+    $content = Read-Host "What are we looking for?: "
+    $file1 = Read-Host "Path to search: "
+    
+    $all = Get-Childitem -Path $file1 -Recurse -Filter *.pdf | ForEach-Object { convert-PDFtoText $_.FullName } | Out-File "C:\ps\bulk.txt"
+
+
+
+    Select-String -Path C:\ps\bulk.txt -Pattern $content | Out-File "C:\ps\select.csv"
+    Select-String -Path C:\ps\bulk.txt -Pattern $content | Out-File "C:\ps\selectbackup.txt"
+
+
+
+    Start-Sleep -Seconds 60
+}
